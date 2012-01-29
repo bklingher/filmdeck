600ee546ffcebb99d23dd0fb3dc0175fcf21d570
Peter Sugihara <peter@campsh.com>
2012-01-29 01:41:26 -0500

    added sign up

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,65 @@ $(function() {
   $('#upload-btn').click(function(e) {
     $('#upload-btn').button('loading');
   });
+
+  $('.error').hide();
+  $('#signup-btn').click(function signup(e) {
+    $('#signup-btn').button('loading');
+
+    var $err = $('#signup-error').hide();
+
+    var name = $("input#signup-username").val();
+    var email = $("input#signup-email").val();
+    var password = $("input#signup-password").val();
+    var err = ''
+    if (name == "") {
+      err = 'Please enter a username.';
+      $("input#signup-username").focus();
+    } else if (email == "") {
+      err = 'Please enter a valid email.';
+      $("input#email").focus();
+    } else if (password.length < 7) {
+      err = 'The password must be at least 7 characters.';
+      $("input#email").focus();
+    }
+
+    if (err != '') {
+      $err.show();
+      $err.html(err);
+      $('#signup-btn').button('reset');
+      e.preventDefault();
+    }
+    this.click(signup);
+  });
+
+
+  $('#sign-in-btn').click(function signup(e) {
+    $('#signup-btn').button('loading');
+
+    var $err = $('#signup-error').hide();
+
+    var name = $("input#signup-username").val();
+    var email = $("input#signup-email").val();
+    var password = $("input#signup-password").val();
+    var err = ''
+    if (name == "") {
+      err = 'Please enter a username.';
+      $("input#signup-username").focus();
+    } else if (email == "") {
+      err = 'Please enter a valid email.';
+      $("input#email").focus();
+    } else if (password.length < 7) {
+      err = 'The password must be at least 7 characters.';
+      $("input#email").focus();
+    }
+
+    if (err != '') {
+      $err.show();
+      $err.html(err);
+      $('#signup-btn').button('reset');
+      e.preventDefault();
+    }
+    this.click(signup);
+  });
+
 });
